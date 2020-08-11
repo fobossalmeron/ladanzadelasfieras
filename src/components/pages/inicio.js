03bbac6b27fad0d93046ef9e613303166979af10
Rodrigo Salmeron <fobos.salmeron@gmail.com>
2020-08-11 16:13:17 -0500

    video back

--- src/components/pages/inicio.js
+++ src/components/pages/inicio.js
@@ -97,8 +97,8 @@ export default function Inicio(props) {
       <Section columnStart="2" columnEnd="8">
         <Fade bottom>
           <TrailerPlayer
-            //url={"https://player.vimeo.com/video/226767311"}
-            url={"https://player.vimeo.com/video/309818457"}
+            url={"https://player.vimeo.com/video/226767311"}
+            // url={"https://player.vimeo.com/video/309818457"}
             still={props.webp ? trailerstillwebp : trailerstill}
             ratio={"56.3%"}
           />
